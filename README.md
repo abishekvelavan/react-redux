bd17620a020fe50bfeec7758d2d3b2426c8fa902
abishekvelavan <abishekvelavan16@gmail.com>
2021-10-01 20:58:59 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-lq7xan
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-lq7xan)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  const name = 'Abishek';
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Hello {name} !</p>
+    </div>
+  );
+}
